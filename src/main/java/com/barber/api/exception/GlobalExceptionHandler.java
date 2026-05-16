fa053e72a08cf4a4dd09df365ae9f5289c894a01
limarobs <rdlp2411@gmail.com>
2026-05-16 09:38:42 -0300

    feat: adiciona tratamento global de exceptions

--- src/main/java/com/barber/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/barber/api/exception/GlobalExceptionHandler.java
@@ -1,17 +1,21 @@
 package com.barber.api.exception;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(HorarioIndisponivelException.class)
     public ResponseEntity erroHorario(HorarioIndisponivelException ex){
         return ResponseEntity.badRequest().body(Map.of("erro", ex.getMessage()));
+    }
 
+    @ExceptionHandler(DataInvalidaException.class)
+    public ResponseEntity dataInvalida(DataInvalidaException ex){
+        return ResponseEntity.badRequest().body(Map.of("erro", ex.getMessage()));
     }
 }
